b09f517cd799eb1ebaf6c5b81af75c2a89f108b6
Rob Kooper <kooper@illinois.edu>
2021-07-18 19:12:20 -0500

    test/cleanup slack notifier

--- notifiers/slack.py
+++ notifiers/slack.py
@@ -12,12 +12,26 @@ class SlackNotifier(HealthNotifier):
         self.user = None if 'user' not in config else config['user']
 
     def notify(self, result):
-        message = ""
-        message += "Label           : %s\n" % result['label'] if 'label' in result else result['label']
-        message += "Status          : %s\n" % result['status']
-        message += '\n'
+        message = "```"
+        message += f"Label           : {result['label']}\n"
+        message += f"Check           : {result['check']}\n"
+        message += f"Status          : {result['status']}\n"
+        if "config" in result:
+            message += f"Config          : {result['config']}\n"
+        message += '```\n'
 
-        requests.post(self.webhook, headers={"Content-Type": "application/json"},
-                      data=json.dumps({"channel": self.channel,
-                                       "username": self.user,
-                                       "text": message}))
+        data = {
+            "channel": self.channel,
+            "username": self.user,
+            "attachments": [
+                {
+                    "fallback": message,
+                    "pretext": result.get("message", f"{result['status']} from {result['check']}-{result['label']}"),
+                    "color": "#2ECC71" if result['status'] == "success" else "#E74C3C",
+                    "text": message
+                }
+            ]
+        }
+        requests.post(self.webhook,
+                      headers={"Content-Type": "application/json"},
+                      data=json.dumps(data))
